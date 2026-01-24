18557e081dece19b95fd18e709d528cc37273be6
remnant song <3143893011@qq.com>
2026-01-24 22:04:24 +0800

    解决useToast.ts的idea提示Vue: Property __TAURI__ does not exist on type Window & typeof globalThis

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
+//vite-env.d.ts
 /// <reference types="vite/client" />
 
 declare module "*.vue" {
@@ -5,3 +6,10 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+// 扩展 Window 接口，添加 __TAURI__ 属性（解决类型报错）
+declare global {
+  interface Window {
+    __TAURI__?: import('@tauri-apps/api').TauriGlobal;
+  }
+}
+export {};
